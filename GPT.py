d112c0be4ece8d03fca93e3137c5f716104a7378
Adib <Adibvafa.fallahpour@mail.utoronto.ca>
2023-10-15 06:57:03 -0400

    Final GPT.py

--- GPT.py
+++ GPT.py
@@ -1,5 +1,6 @@
-import os, requests, json, openai, PyPDF2, pdfplumber, re, tiktoken
+import os, requests, json, openai, PyPDF2, pdfplumber, re, tiktoken, asyncio, aiohttp
 from dotenv import load_dotenv;
+import summarizer
 
 load_dotenv()
 from io import BytesIO
@@ -10,48 +11,28 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 class SectionNode:
 
-    def __init__(self, title, start):
+    def __init__(self, title, page, top):
         self.id = str(id(self))
-        self.start = start
         self.title = title
         self.text = ""
         self.children = []
-        self.json = {}
-
-        self.getText()
-        self.getSummary()
+        self.page = page
+        self.top = top
+        self.depth = None
 
     def addChild(self, other):
         self.children.append(other)
 
-    def getJson(self):
-        self.json[self.id] = {}
-        self.json[self.id]["start"] = self.start
-        self.json[self.id]["title"] = self.title
-        self.json[self.id]["text"] = self.text
-        self.json[self.id]["children"] = [child.id for child in self.children]
-        return self.json
-
-    def getText(self):
-        # TODO Use PyPDF to get the text based on self.start
-        self.text = self.title
-
-    def getSummary(self):
-        # TODO Use GPT to get the summary of text
-        self.text = self.title
-
     def __str__(self):
-        return f'Title: {self.title}\n' \
+        return f'Depth: {self.depth}\n' \
+               f'Title: {self.title}\n' \
+               f'Page: {self.page}\n' \
                f'Text: {self.text}\n' \
                f'Children: {[child.title for child in self.children]}\n'
 
 
-def create_graph(pdf_path):
-    pdf_file = open(pdf_path, 'rb')
-    pdf_reader = PyPDF2.PdfReader(pdf_file)
-
-    # TODO Use GPT to get the root note information right
-    root = SectionNode("Attention Is All You Need", "START")
+def create_graph(pdf_reader, title, page):
+    root = SectionNode(title, page, None)
     get_bookmark(root, pdf_reader.outline)
 
     return root
@@ -61,22 +42,176 @@ def get_bookmark(root, outline):
     new = None
 
     for item in outline:
-
         if isinstance(item, list):
             get_bookmark(new, item)
         else:
-            new = SectionNode(item.title, item.page)
+            new = SectionNode(item.title, item.page, item.top)
             root.addChild(new)
 
 
 def get_json_output(json_output, root):
-    json_output.append(root.getJson())
+    json_output[root.id] = {}
+    json_output[root.id]["depth"] = root.depth
+    json_output[root.id]["page"] = root.page
+    json_output[root.id]["top"] = root.top
+    json_output[root.id]["title"] = root.title
+    json_output[root.id]["text"] = root.text
+    json_output[root.id]["children"] = [child.id for child in root.children]
+
     for child in root.children:
         get_json_output(json_output, child)
 
 
-def main(pdf_path):
-    json_output = []
-    root = create_graph(pdf_path)
+def get_text_from_page(page, bottom, up):
+    def visitor_body_with_range(bottom, up):
+        def visitor_body(text, cm, tm, fontDict, fontSize):
+            y = tm[5]
+            if bottom < y < up:
+                parts.append(text)
+
+        return visitor_body
+
+    parts = []
+    page.extract_text(visitor_text=visitor_body_with_range(bottom=bottom, up=up))
+    return "".join(parts).strip()
+
+
+def get_all_nodes(all_nodes, root):
+    all_nodes.append(root)
+    for child in root.children:
+        get_all_nodes(all_nodes, child)
+
+
+def get_page_number(pdf_reader, node):
+    if type(node.page) == PyPDF2.PageObject:
+        return pdf_reader.get_page_number(node.page)
+    return pdf_reader.get_page_number(pdf_reader.get_object(node.page))
+
+
+def adjust_page_numbers(pdf_reader, all_nodes):
+    for node in all_nodes:
+        node.page = get_page_number(pdf_reader, node)
+
+
+def float2string(all_nodes):
+    for node in all_nodes:
+        node.page = str(node.page)
+        node.top = str(node.top)
+        node.depth = str(node.depth)
+
+
+def get_text_all_nodes(pdf_reader, all_nodes):
+    prev_i = 1
+    next_i = 2
+
+    while next_i < len(all_nodes):
+
+        prev_node = all_nodes[prev_i]
+        next_node = all_nodes[next_i]
+        page = pdf_reader.pages[prev_node.page]
+
+        if prev_node.page == next_node.page:
+            text = get_text_from_page(page, bottom=next_node.top, up=prev_node.top).strip()
+            prev_node.text = text[text.find('\n') + 1:].replace('\n', ' ')
+
+        else:
+            text = get_text_from_page(page, bottom=42, up=prev_node.top).strip()
+            text = text[text.find('\n') + 1:].replace('\n', ' ')
+
+            curr_page_num = prev_node.page + 1
+
+            while curr_page_num < next_node.page:
+                text += pdf_reader.pages[curr_page_num].extract_text().replace('\n', ' ')
+                curr_page_num += 1
+
+            text += get_text_from_page(pdf_reader.pages[curr_page_num], bottom=next_node.top, up=750).replace('\n', ' ')
+            prev_node.text = text
+
+        next_i, prev_i = next_i + 1, prev_i + 1
+
+    # For the last node
+    prev_node = all_nodes[prev_i]
+    page = pdf_reader.pages[prev_node.page]
+    text = get_text_from_page(page, bottom=50, up=prev_node.top).strip()
+    text = text[text.find('\n') + 1:].replace('\n', ' ')
+
+    curr_page_num = prev_node.page + 1
+    while curr_page_num < len(pdf_reader.pages):
+        text += pdf_reader.pages[curr_page_num].extract_text().replace('\n', ' ')
+        curr_page_num += 1
+
+    prev_node.text = text[:text.find("References")]
+
+
+def get_title_root(pdf_reader):
+    prompt = f"What is the title of this article?\n{pdf_reader.pages[0].extract_text()}\nTitle: "
+    response, _ = ask_chatgpt(prompt, history=[], system=None, new_chat=True, max_tokens=50, temp=0)
+    return response.strip()
+
+
+def get_depth(root, depth):
+    root.depth = depth
+    for child in root.children:
+        get_depth(child, depth + 1)
+
+
+def user_said(content, history):
+    history.append({"role": "user", "content": content})
+
+
+def assistant_said(content, history):
+    history.append({"role": "assistant", "content": content})
+
+
+def ask_chatgpt(user, history, system=None, new_chat=False, max_tokens=256, only_response=False, temp=0,
+                model='gpt-3.5-turbo'):
+    history = [] if new_chat else history
+
+    if system and new_chat:
+        history.append({"role": "system", "content": system})
+    user_said(user, history)
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=history,
+        temperature=temp,
+        max_tokens=max_tokens,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    response = response['choices'][0]['message']['content']
+
+    if only_response:
+        return response
+    else:
+        assistant_said(response, history)
+        return response, history
+
+
+def main(opened_pdf):
+    pdf_reader = PyPDF2.PdfReader(opened_pdf)
+    root = create_graph(pdf_reader, title=get_title_root(pdf_reader), page=pdf_reader.pages[0])
+
+    all_nodes = []
+    json_output = {}
+
+    get_all_nodes(all_nodes, root)
+    get_depth(root, 0)
+    adjust_page_numbers(pdf_reader, all_nodes)
+    get_text_all_nodes(pdf_reader, all_nodes)
+    # float2string(all_nodes)
+
+    section_texts = [node.text for node in all_nodes]
+    summarized_texts = asyncio.run(summarizer.main(section_texts))
+
+    for i, each_summary in enumerate(summarized_texts):
+        all_nodes[i].text = each_summary
+
     get_json_output(json_output, root)
     return json_output
+
+
+if __name__ == '__main__':
+    json_output = main(open('attention.pdf', 'rb'))
+    print(json_output)
---
+++ summarizer.py
@@ -0,0 +1,50 @@
+import os, aiohttp, asyncio, openai
+from dotenv import load_dotenv
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+
+async def summarize(session, section, max_tokens=280, temp=0):
+    system = f'Act as a professional scientist that reviews articles.'
+    article = f'article: {section}'
+    prompt = f'{system} {article}.\nExplain all important information, terms, and ideas in summarized bullet points: '
+
+    history = [{"role": "system", "content": system},
+               {"role": "user", "content": prompt}]
+
+    url = 'https://api.openai.com/v1/chat/completions'
+    headers = {
+        'Authorization': f'Bearer {openai.api_key}',
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'model': 'gpt-3.5-turbo',
+        'messages': history,
+        'temperature': temp,
+        'max_tokens': max_tokens,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+    }
+
+    async with session.post(url, json=payload, headers=headers) as response:
+        response_json = await response.json()
+        if response.status == 200:
+            return response_json['choices'][0]['message']['content']
+        else:
+            print(f'Error: {response_json}')
+            return None
+
+
+async def main(sections):
+    # sections = ['Hello this is biology', 'Hello this is chemistry', 'hello this is computer science and i would
+    # like to talk about ai']   # List of sections to be summarized
+    async with aiohttp.ClientSession() as session:
+        summaries = await asyncio.gather(*(summarize(session, section) for section in sections))
+        # for i, summary in enumerate(summaries):
+        #     print(f'Summary {i + 1}:\n{summary}\n')
+    return summaries
+
+# Run the main function
+# if __name__ == '__main__':
+#     asyncio.run(main())
