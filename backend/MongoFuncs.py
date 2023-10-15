c727481a4b6b78e8edb94c670a73528351be50d3
Aarya2004 <aarya.prakash.2004@gmail.com>
2023-10-15 02:19:56 -0400

    Still working on frontend-backend interaction, framework created, need Adib's file

--- backend/MongoFuncs.py
+++ backend/MongoFuncs.py
@@ -3,24 +3,44 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 from bson.json_util import dumps
+import json
 
 load_dotenv()
-USERNAME = os.environ.get("USERNAME")
-PASSWORD = os.environ.get("PASSWORD")
-uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@users.jvsjo1k.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-# Send a ping to confirm a successful connection
-collection = client["All_Articles"]
-db = collection["Articles"]
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
 
-def queryMongo(id, db):
-    query = {"id": str(id)}
-    result = dict(db.find(query)[0])
-    json_result = dumps(result)
-    return json_result
+class DBQuery():
+
+    def __init__(self):
+        USERNAME = os.environ.get("USERNAME")
+        PASSWORD = os.environ.get("PASSWORD")
+        uri = f"mongodb+srv://{USERNAME}:{PASSWORD}@users.jvsjo1k.mongodb.net/?retryWrites=true&w=majority"
+        # Create a new client and connect to the server
+        client = MongoClient(uri, server_api=ServerApi('1'))
+        # Send a ping to confirm a successful connection
+        collection = client["All_Articles"]
+        self.db = collection["Articles"]
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+    def queryMongo(self, id):
+        json_result = {}
+        try:
+            query = {"id": str(id)}
+            result = dict(self.db.find(query)[0])
+            json_result = dumps(result)
+        except Exception as e:
+            pass
+        return json_result
+
+    def currentIndex(self):
+        result = list(self.db.find())
+        id = result[len(result)-1]["id"]
+        return id
+
+    def insertMongo(self, input_json):
+        try:
+            self.db.insert_one(input_json)
+        except Exception as e:
+            pass
--- backend/api.py
+++ backend/api.py
@@ -1,6 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import GPT as gpt
+import MongoFuncs as mf
 
 app = Flask(__name__)
 
 if __name__ == "__main__":
   app.run(debug=True)
+
+@app.route('/upload', methods=['POST'])
+def sendPdf():
+  if request.method == 'POST':
+    if 'file' not in request.files:
+        return jsonify({})
+
+    file = request.files['file']
+
+  if file.filename == '':
+    return jsonify({})
+  
+  if file:
+    db = mf.DBQuery()
+    result_json = jsonify({})
+    nodeList = gpt.main(file)
+    first_id = list(nodeList[0].keys())[0]
+    articleTitle = nodeList[0][first_id]["title"]
+    articleText = nodeList[0][first_id]["text"]
+    id = int(db.currentIndex()) + 1
+    if len(db.queryMongo(id)) == 0:
+      result_json = jsonify({"id": str(id), "title": articleTitle, "description": articleText, "nodes":nodeList})
+    db.insertMongo(result_json)
+    return result_json
+
+@app.route('/<question>', methods=['POST'])
+def askGPT(question):
+  if request.method == 'POST':
+    return GPT(question)
+    
