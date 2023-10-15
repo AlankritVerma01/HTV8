34d7d9575060ec1901e169f3ac812ac23244fc79
Adib <Adibvafa.fallahpour@mail.utoronto.ca>
2023-10-15 09:16:55 -0400

    Implemented answer_question

--- GPT.py
+++ GPT.py
@@ -150,6 +150,12 @@ def get_title_root(pdf_reader):
     return response.strip()
 
 
+def get_text_root(pdf_reader):
+    prompt = f"What is the abstract of this article?\n{pdf_reader.pages[0].extract_text()}\nAbstract: "
+    response, _ = ask_chatgpt(prompt, history=[], system=None, new_chat=True, max_tokens=250, temp=0)
+    return response.strip()
+
+
 def get_depth(root, depth):
     root.depth = depth
     for child in root.children:
@@ -200,9 +206,29 @@ def ask_chatgpt(user, history, system=None, new_chat=False, max_tokens=256, only
         return response, history
 
 
+def answer_question(section, question, max_tokens=150, temp=0):
+
+    system = f'Act as a professional scientist that reviews articles.'
+    prompt = f'article: {section}.\nQuestion: {question}\nAnswer: '
+
+    history = [{"role": "system", "content": system}, {"role": "user", "content": prompt}]
+
+    response = openai.ChatCompletion.create(
+      model='gpt-3.5-turbo',
+      messages=history,
+      temperature=temp,
+      max_tokens=max_tokens,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+    return response['choices'][0]['message']['content']
+
+
 def main(opened_pdf):
     pdf_reader = PyPDF2.PdfReader(opened_pdf)
     root = create_graph(pdf_reader, title=get_title_root(pdf_reader), page=pdf_reader.pages[0])
+    root.text = get_text_root(pdf_reader)
 
     all_nodes = []
     json_output = {}
